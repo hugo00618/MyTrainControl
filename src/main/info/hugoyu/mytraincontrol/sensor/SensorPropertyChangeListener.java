fccd4922a64955f7227ca00d4fda0467eb32037c
Hugo Yu <guo.yu@uwaterloo.ca>
2021-12-04 11:37:19 -0800

    Optimization for flickering sensors

--- src/main/info/hugoyu/mytraincontrol/sensor/SensorPropertyChangeListener.java
+++ src/main/info/hugoyu/mytraincontrol/sensor/SensorPropertyChangeListener.java
@@ -1,26 +1,50 @@
 package info.hugoyu.mytraincontrol.sensor;
 
 import jmri.Sensor;
-import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SensorPropertyChangeListener implements PropertyChangeListener {
 
+    private static long FLICKER_THRESHOLD_MILLIS = 1000;
+
+    @NonNull
     private Sensor sensor;
 
+    @NonNull
     private SensorChangeListener listener;
+
+    private long lastUpdatedTime = 0;
+
+    private boolean hasFlickered = false;
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        int newVal = (int) evt.getNewValue();
+        boolean isFlicker = false;
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastUpdatedTime < FLICKER_THRESHOLD_MILLIS) {
+            isFlicker = true;
+            hasFlickered = true;
+        }
+        lastUpdatedTime = currentTime;
 
+        if (!isFlicker) {
+            updateListener((int) evt.getNewValue());
+        }
+    }
+
+    private void updateListener(int newVal) {
         if (newVal == Sensor.ACTIVE) {
             listener.onEnter(sensor);
         } else if (newVal == Sensor.INACTIVE) {
-            listener.onExit(sensor);
+            // do not react to LOW events on flickering sensors
+            if (!hasFlickered) {
+                listener.onExit(sensor);
+            }
         }
     }
 
